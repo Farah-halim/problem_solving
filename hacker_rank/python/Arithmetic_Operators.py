dccd074fe5a4c68ec9f9e70226c467275f414213
farah-halim <level2bis2@gmail.com>
2023-09-14 14:58:36 +0300

    Arithmetic Operators in python

---
+++ hacker_rank/python/Arithmetic_Operators.py
@@ -0,0 +1,16 @@
+""" Task
+The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
+
+1- The first line contains the sum of the two numbers.
+2- The second line contains the difference of the two numbers (first - second).
+3- The third line contains the product of the two numbers.
+"""
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    sum =a+b
+    difference = a-b
+    products = a*b
+    print(sum)
+    print(difference)
+    print (products)
